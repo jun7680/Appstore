c5d2e48b281201400f228755296f3accb837719a
jun7680 <injunock@naver.com>
2023-03-20 22:41:34 +0900

    Add Extension

--- Appstore/Appstore/Extensions/UIImageViewExtension.swift
+++ Appstore/Appstore/Extensions/UIImageViewExtension.swift
@@ -5,4 +5,15 @@
 //  Created by 옥인준 on 2023/03/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        ImageCache.shared.requestImage(url: url) { [weak self] image in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+}
--- Appstore/Appstore/Extensions/UIViewExtension.swift
+++ Appstore/Appstore/Extensions/UIViewExtension.swift
@@ -12,3 +12,9 @@ extension UIView {
         views.forEach { addSubview($0) }
     }
 }
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: [UIView]) {
+        views.forEach { addArrangedSubview($0) }
+    }
+}
